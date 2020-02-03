9758c74eff4163fdd7ab6d563627c69bc53ea450
husharon <husharon@me.com>
2020-02-03 13:55:14 +0100

    scripts folder w/ PlayerController

---
+++ Assets/Scripts/PuppyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuppyController : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float y = Input.GetAxis("Vertical") * speed;
+
+        rb.velocity = new Vector2(x, y);
+    }
+}
