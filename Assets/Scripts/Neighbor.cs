17b35a44d084b7ab09f97b7c5ef38da129f4e6b0
wpramz <williampramono7@gmail.com>
2020-02-03 15:14:28 +0100

    added target detection

---
+++ Assets/Scripts/Neighbor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neighbor : MonoBehaviour
+{
+
+    private float angle = 20.0f; // rotate at angle degrees/second
+    private Vector3 axis = Vector3.forward; // rotation axis
+    private Vector3 neighborDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        neighborDirection = Vector3.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update neighbor direction by a few degrees every frame
+        transform.RotateAround(Vector3.zero, axis, angle * Time.deltaTime);
+
+        if (CanSeePuppy())
+        {
+            // do something in PuppyController ? 
+            // start level over ?
+        }
+       
+    }
+
+    bool CanSeePuppy()
+    {
+        RaycastHit2D hit;
+
+
+        return false;
+    }
+}
