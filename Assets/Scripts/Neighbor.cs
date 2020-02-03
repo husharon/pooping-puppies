8c8a9e846ce71eb4eb207d55f51539fd589d1cc6
Michelle <michenicholas@gmail.com>
2020-02-03 14:28:15 +0100

    added Michelle scene

---
+++ Assets/Scripts/Neighbor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neighbor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float angle = 360.0f; // rotate 360 degrees
+    float time = 1.0f; // Time unit in sec
+    Vector3 axis = Vector3.up; // rotation axis
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update neighbor direction by a few degrees every frame
+        gameObject.GetComponent<Transform>().RotateAround(Vector3.zero, axis, angle * Time.deltaTime / time);
+        
+    }
+
+    bool CanSeePuppy()
+    {
+        RaycastHit2D hit;
+        Vector2 neighborDirection;
+
+
+
+    }
+}
