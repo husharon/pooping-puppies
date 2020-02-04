f73028da5ada203d86b536aa09d5f162c8dbcbba
Michelle <michenicholas@gmail.com>
2020-02-04 13:50:35 +0100

    updated puppy detection

--- Assets/Scripts/Cone.cs
+++ Assets/Scripts/Cone.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Cone : MonoBehaviour
+public class Cone : Neighbor
 {
     private GameObject cat;
-    private float distance = 5.0f; // distance neighbor can see
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +23,14 @@ public class Cone : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            Vector2 dogPos = col.gameObject.transform.position;
-            Vector2 catPos = cat.transform.position;
-            RaycastHit2D hit = Physics2D.Raycast(catPos, dogPos - catPos);
-            if (hit.collider.CompareTag("Player"))
+            Debug.Log("Message: Collided with the player, there might be a bush in the way");
+            if (CanSeePuppy(col.gameObject.transform.position))
             {
-                Debug.Log("cat sees dog");
-                Debug.Log(hit.transform.tag);
+                Debug.Log("Message: Puppy has been seen, game should end");
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
+            
+        
         }
     }
 }
--- Assets/Scripts/Neighbor.cs
+++ Assets/Scripts/Neighbor.cs
@@ -8,60 +8,31 @@ public class Neighbor : MonoBehaviour
 
     //private float angle = 20.0f; // rotate at angle degrees/second
     private float speed = 3.0f;
-    private float distance = 5.0f;
     private Vector3 axis = Vector3.forward; // rotation axis
-    private Vector3 neighborDirection;
-    private Vector3 origin;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        origin = transform.position;
-    }
 
     // Update is called once per frame
     void Update()
     {
         // update neighbor direction by a few degrees every frame
         transform.Rotate(axis * speed * Time.deltaTime); // this rotates the actual object, not a vector
-
-        //transform.rotation()
-
-        // update neighborDirection variable
-        neighborDirection = transform.right;
-        
-
-
-        // use quaternion with neighborDirection ?
-
-        
-
-        if (CanSeePuppy())
-        {
-            // do something in PuppyController ? 
-            // start level over ?
-
-            // player dies, game over
-            Debug.Log("Detected puppy");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
        
     }
-
-    bool CanSeePuppy()
+    
+    public bool CanSeePuppy(Vector3 dogPos)
     {
+        Vector3 dirToDog = dogPos - transform.position;
         
-        RaycastHit2D hit = Physics2D.Raycast(origin, neighborDirection, distance);
-        Debug.DrawRay(origin, neighborDirection, Color.red, 25.0f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dirToDog);
+        Debug.DrawRay(transform.position, dirToDog, Color.red, 25.0f);
 
      
         if (hit.collider != null)
         {
-            Debug.Log("Hit an object: " + hit.transform.tag);
+            Debug.Log("Message: Raycast hit an object: " + hit.transform.tag);
             
             if (hit.collider.CompareTag("Player"))
             {
-                Debug.Log("Hit player");
+                Debug.Log("Message: Hit player in CanSeePuppy");
                 return true;
             }
         }
